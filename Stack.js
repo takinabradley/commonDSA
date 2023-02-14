79f26d964eadd3f467b0ca5c250fb9e5bd48ed2d
takinabradley <bradfinney98@gmail.com>
2023-02-14 05:19:00 -0500

    Create a limited stack

--- Stack.js
+++ Stack.js
@@ -8,7 +8,7 @@ function StackNode(value) {
 function Stack() {
   let top = null
 
-  function push(value) {
+  const push = (value) => {
     const newNode = StackNode(value)
     if(top === null) {
       top = newNode
@@ -19,7 +19,7 @@ function Stack() {
     }
   }
 
-  function pop() {
+  const pop = () => {
     if(top === null) {
       return null
     } else if(top.next === null) {
@@ -37,5 +37,50 @@ function Stack() {
     top: {enumerable: true, get() {return top}},
     push: {enumerable: true, value: push},
     pop: {enumerable: true, value: pop}
+  })
+}
+
+function LimitedStack(size) {
+  if(!size) throw TypeError('LimitedStack must have a maximum size!')
+  let top = null
+  let currentSize = 0
+
+  const isFull = () => currentSize === size
+  const push = (value) => {
+    if(isFull()) throw Error('Stack is full!')
+    const newNode = StackNode(value)
+    if(top === null) {
+      top = newNode
+    } else {
+      const temp = top
+      top = newNode
+      newNode.next = temp
+    }
+    return ++currentSize
+  }
+
+  const pop = () => {
+    if(top === null) {
+      return null
+    } else if(top.next === null) {
+      --currentSize
+      const node = top
+      top = null
+      return node.value
+    } else {
+      --currentSize
+      const node = top
+      top = top.next
+      return node.value
+    }
+  }
+
+  return Object.create(null, {
+    top: {enumerable: true, get() {return top}},
+    push: {enumerable: true, value: push},
+    pop: {enumerable: true, value: pop},
+    isFull: {enumerable: true, value: isFull},
+    size: {enumerable: true, value: size},
+    currentSize: {enumerable: true, get() {return currentSize}}
   })
 }
